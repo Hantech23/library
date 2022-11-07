3deff1258bcf74bcf2ba5a0b207b8adfa8e4c820
Woobin Han <woobin.han23@gmail.com>
2022-11-07 04:54:59 -0500

    Developed display function

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
 let library = [];
 
-function Book (title, author, read) {
-    this.title = title;
-    this.author = author;
-    this.read = read;
-    this.info = function() {
-        if(read == true) {
-            return(title + " by " + author + ", " + " read")
-        }
-        else {
-            return(title + " by " + author + ", " + " not read")
-        }
-        
-    }
-}
-
-//Add to array
-function addBookToLibrary(Book) {
-    library.push(Book)
-}
-
 const book1 = new Book('Heart of Darkness', 'Joseph Conrad', 90, true)
 const book2 = new Book('Artemis Fowl', 'Eoin Colfer', 100, true)
 const book3 = new Book('Konzertstuck', 'Carl Maria von Weber', 30, true)
@@ -28,18 +8,41 @@ addBookToLibrary(book2);
 addBookToLibrary(book3);
 
 addBookToLibrary(new Book('Can I skip', 'a few steps', 2, false))
+addBookToLibrary(new Book('This should be in the next line', 'author', 2, false))
 
+function Book (title, author, read) {
+    this.title = title;
+    this.author = author;
+    this.read = read;
+}
 
+function addBookToLibrary(Book) {
+    library.push(Book)
+}
 
 const bookShelf = document.querySelector('.book-shelf');
+
+
 function display() {
     for (let eachBook of library) {
-        console.log(eachBook.info());
-
-        const book = document.createElement('div');
-        book.textContent = eachBook.info();
-        bookShelf.append(book);
+        createBookCard(eachBook)
     }
 }
 
 display();
+
+
+function createBookCard(Book) {
+    const bookCard = document.createElement('div')
+    const title = document.createElement('p')
+    const author = document.createElement('p')
+
+    bookCard.classList.add('book-card')
+    title.textContent = Book.title;
+    author.textContent = Book.author;
+    
+    bookCard.append(title)
+    bookCard.append(author)
+    bookShelf.append(bookCard)
+}
+
